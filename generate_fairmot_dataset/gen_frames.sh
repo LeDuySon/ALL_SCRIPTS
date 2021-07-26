4ab6e1186b64f2db9c4f5b498b146ffd192ce5a4
Son <leduyson2632001@gmail.com>
2021-07-27 00:00:49 +0700

    update gen frame scripts

--- generate_fairmot_dataset/gen_frames.sh
+++ generate_fairmot_dataset/gen_frames.sh
@@ -1,8 +1,7 @@
 video_path=$1
 save_path=$2
-mode=$3
 for video in ${video_path}/*.mp4; do
     echo $video
     echo "TEST"
-    python generate_fairmot_dataset.py --video_path $video --save_path $save_path --mode $mode
+    python generate_fairmot_dataset.py --video_path $video --save_path $save_path
 done
