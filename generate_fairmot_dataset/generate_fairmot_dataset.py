e0d550c77fb40cf524a0bff2c16673943e0e7d0d
Son <leduyson2632001@gmail.com>
2021-07-26 23:59:32 +0700

    modify generate frame

--- generate_fairmot_dataset/generate_fairmot_dataset.py
+++ generate_fairmot_dataset/generate_fairmot_dataset.py
@@ -3,9 +3,7 @@ import math
 import numpy as np
 import argparse
 import os
-# Make sure that the print function works on Python 2 and 3
 from del_noobj_frame import get_frame_have_objects
-# Capture every n seconds (here, n = 5) 
 parser = argparse.ArgumentParser(description='Process some integers.')
 parser.add_argument('--video_path', '-V', type=str,
                     help='video path to extract frame')
@@ -15,12 +13,11 @@ parser.add_argument('--save_path', '-sp', type=str,
 # this arg decides whether or not save frame that dont have any object in it. 
 # Train mode: not save frame cuz it will not find txt file for this frame -> error when training
 # test mode: save frame to calculate framerate exactly
-parser.add_argument('--mode', type=str, help="train or test")
+#parser.add_argument('--mode', type=str, help="train or test")
 
 
 args = parser.parse_args()
 
-#################### Setting up the file ################
 videoFile = args.video_path
 vidcap = cv2.VideoCapture(videoFile)
 success, image = vidcap.read()
@@ -36,9 +33,9 @@ if(not os.path.exists(args.save_path)):
 
 tmp_name = args.video_path.split("/")
 cat_name = "/".join(tmp_name[-2:])[:-4]
-label_path = "/mnt/1T/TRAIN_DATASET/fairmot_dataset/UET_MOT/labels_with_ids/{}/img1".format(cat_name)
+#label_path = "/mnt/1T/TRAIN_DATASET/fairmot_dataset/UET_MOT/labels_with_ids/{}/img1".format(cat_name)
 
-valid_frames = get_frame_have_objects(label_path)
+#valid_frames = get_frame_have_objects(label_path)
 #################### Initiate Process ################
 while success:
     frameId = int(round(vidcap.get(1))) - 1 #current frame number, rounded b/c sometimes you get frame intervals which aren't integers...this adds a little imprecision but is likely good enough
@@ -47,10 +44,10 @@ while success:
     print(frameId)
     success, image = vidcap.read()
     num_zeros = 6 - len(str(frameId))
-    save_img_name = "frame_" + "0" * num_zeros + str(frameId) + ".jpg"
+    save_img_name = "frame_" + "0" * num_zeros + str(frameId) + ".jpg" # frame name format: frame_xxxxxx.jpg
     save_name = save_img_name.split(".")[0]
-    if(save_name not in valid_frames and args.mode == "train"):
-        continue    
+ #   if(save_name not in valid_frames and args.mode == "train"):
+ #       continue    
     save_file = os.path.join(args.save_path, save_img_name)
     cv2.imwrite("%s" % save_file, image)
         
